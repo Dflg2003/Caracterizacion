9bc0546b1acae8ef18d034bcc85ac70552ccfc64
MiltonStivenPinzon1223 <107333427+MiltonStivenPinzon1223@users.noreply.github.com>
2022-12-22 14:10:50 -0500

    cambios faltantes

--- php/page7.php
+++ php/page7.php
@@ -38,6 +38,7 @@ echo $sql;
 $consulta = mysqli_query($con, $sql);
 
 if ($consulta > 0) {
+    //De aca nos debe votar a la ultima del formulario no a la pagina 8
     header("Location: page8.php?documento=$documento"   );
 }
 
