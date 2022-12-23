2484631c02691d14783e2c4451d33899f9827130
dflg2003 <daniel.felipe.lg@hotmail.com>
2022-12-23 15:31:12 -0500

    p10

--- php/page8.php
+++ php/page8.php
@@ -43,7 +43,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <hr>
         <label for="sustancias" class="form-label">Consumo mucha azúcar, sal, comida chatarra o grasosa: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta75" required>
-            <option value=""></option>
+            
             <option value="Ninguna de estas">Ninguna de estas</option>
             <option value="Alguna de estas">Alguna de estas</option>
             <option value="Todas estas">Todas estas</option>
@@ -52,7 +52,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <br>
         <label for="sustancias" class="form-label">En cuanto a mi peso ideal: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta76" required>
-            <option value=""></option>
+            
             <option value="Estoy normal o 4 kilos por encima o por debajo">Estoy normal o 4 kilos por encima o por debajo</option>
             <option value="De 5 a 8 kilos por encima o por debajo">De 5 a 8 kilos por encima o por debajo</option>
             <option value="Más de 8 kilos por encima o por debajo">Más de 8 kilos por encima o por debajo</option>
@@ -67,7 +67,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
 
         <label for="sustancias" class="form-label">Perteneces a alguno de los siguientes grupos o poblaciones: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta78"  required>
-            <option value=""></option>
+            
             <option value="Victima del conflicto armado">Victima del conflicto armado</option>
             <option value="Indigena">Indigena</option>
             <option value="Afrocolombiano">Afrocolombiano</option>
@@ -85,7 +85,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Cafeína: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta79"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -96,7 +96,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Marihuana: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta80"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -107,7 +107,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Cigarrillo: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta81"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -118,7 +118,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Alcohól: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta82"  required> 
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -128,7 +128,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Cocaína: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta83"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -139,7 +139,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">LSD: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta84"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -150,7 +150,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Anfetamina, Metanfetamina, Extasis: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta85"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -161,7 +161,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Inhalables (Popper): </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta86"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -172,7 +172,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Medicamentos no recetados: </label>
         <select class="btn btn-outline-dark"  class="form-select" name="pregunta87"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -183,7 +183,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
             <label for="sustancias" class="form-label">Bebidas enegizantes: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta88"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="Esporádicamente">Esporádicamente</option>
             <option value="mensualmente">mensualmente</option>
@@ -194,7 +194,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Si consumes café, ¿Cuántas tazas consumes al día?: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta89"  required>
-            <option value=""></option>
+            
             <option value="Menos de 3 por día">Menos de 3 por día</option>
             <option value="Entre 4 y 6">Entre 4 y 6</option>
             <option value="Más de 6 al día">Más de 6 al día</option>
@@ -205,7 +205,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Si consumes cigarrillo, ¿Cuántos consumes al día?: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta90"  required>
-            <option value=""></option>
+            
             <option value="1">1</option>
             <option value="Entre 2 a 5">Entre 2 a 5</option>
             <option value="Más de 5">Más de 5</option>
@@ -226,7 +226,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Bebo más de 4 tragos en una ocasión: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta92"  required>
-            <option value=""></option>
+            
             <option value="Nunca">Nunca</option>
             <option value="ocasionalmente">ocasionalmente </option>
             <option value="A menudo">A menudo</option>
@@ -236,7 +236,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
         <div class="botones">
         <label for="sustancias" class="form-label">Suelo conducir después de consumir alguna bebida alcohólica: </label>
         <select class="btn btn-outline-dark" class="form-select" name="pregunta93"  required>
-            <option value=""></option>
+            
             <option value="Siempre">Siempre</option>
             <option value="A veces">solo rara vez</option>
             <option value="A menudo">A menudo</option>
@@ -251,7 +251,7 @@ echo "<input type='hidden' name='documento' value='$documento'>";
             <option value="Siempre">Siempre</option>
             <option value="A veces">A veces</option>
             <option value="Casi nunca">Casi nunca</option>
-        </select>
+        </select><br><br>
         </div>
         <center><button type="submit" class="btn btn-primary">Siguiente Pagina <i class="fa fa-angle-right"></i></button> </center>
 
