f013c1925c30f836ce26afd4bdcb298833e7e9c8
dflg2003 <daniel.felipe.lg@hotmail.com>
2022-12-22 21:10:43 -0500

    si

---
+++ php/page10.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../Style/parallax.css">
+
+    <link rel="stylesheet" href="../Style/menu.css">
+
+    <link rel="stylesheet" href="../Style/Slider.css">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="../">
+
+    <title>Finalizacion</title>
+
+</head>
+
+<body class="fondo">
+
+    <header>
+
+    <nav>
+
+        <input type="checkbox" id="check">
+
+        <label for="check" class="checkbtn">
+
+            <i class="fas fa-bars"></i>
+
+        </label>
+
+        <a href="#" class="enlace">
+
+            <img src="../assets/img/LogoUni.jpg" alt="" class="logo">
+
+        </a>
+
+        <ul>
+
+            <li><a class="active" href="#">Formulario General de ingreso</a></li>
+
+           
+
+        </ul>
+
+    </nav>
+
+</header>
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<Script src="../Script/gracias.js"></Script>
+
+
+
+
+</body>
+
+</html>
--- php/page9.php
+++ php/page9.php
@@ -1,72 +1,38 @@
-<!DOCTYPE html>
+<?php
 
-<html lang="en">
+include 'conexion.php';
 
-<head>
+$documento = $_POST['documento'];
+$pregunta75 = $_POST['pregunta75'];
+$pregunta76 = $_POST['pregunta76'];
+$pregunta77 = $_POST['pregunta77'];
+$pregunta78 = $_POST['pregunta78'];
+$pregunta79 = $_POST['pregunta79'];
+$pregunta80 = $_POST['pregunta80'];
+$pregunta81 = $_POST['pregunta81'];
+$pregunta82 = $_POST['pregunta82'];
+$pregunta83 = $_POST['pregunta83'];
+$pregunta84 = $_POST['pregunta84'];
+$pregunta85 = $_POST['pregunta85'];
+$pregunta86 = $_POST['pregunta86'];
+$pregunta87 = $_POST['pregunta87'];
+$pregunta88 = $_POST['pregunta88'];
+$pregunta89 = $_POST['pregunta89'];
+$pregunta90 = $_POST['pregunta90'];
+$pregunta91 = $_POST['pregunta91'];
+$pregunta92 = $_POST['pregunta92'];
+$pregunta93 = $_POST['pregunta93'];
+$pregunta94 = $_POST['pregunta94'];
 
-    <meta charset="UTF-8">
+echo $documento;
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+$sql = "UPDATE respuestas SET pregunta75 = '$pregunta75', pregunta76 = '$pregunta76', pregunta77 = '$pregunta77', pregunta78 = '$pregunta78', pregunta79 = '$pregunta79', pregunta80 = '$pregunta80', pregunta81 = '$pregunta81', pregunta82 = '$pregunta82', pregunta83 = '$pregunta83', pregunta84 = '$pregunta84', pregunta85 = '$pregunta85', pregunta86 = '$pregunta86', pregunta87 = '$pregunta87', pregunta88 = '$pregunta88', pregunta89 = '$pregunta89', pregunta90 = '$pregunta90', pregunta91 = '$pregunta91', pregunta92 = '$pregunta92', pregunta93 = '$pregunta93', pregunta94 = '$pregunta94', WHERE documento = '$documento'";
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+echo $sql;
+$consulta = mysqli_query($con, $sql);
 
-    <link rel="stylesheet" href="../Style/parallax.css">
+if ($consulta > 0) {
+    header("Location: page10.php?documento=$documento"   );
+}
 
-    <link rel="stylesheet" href="../Style/menu.css">
-
-    <link rel="stylesheet" href="../Style/Slider.css">
-
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
-    <link rel="stylesheet" href="../">
-
-    <title>Finalizacion</title>
-
-</head>
-
-<body class="fondo">
-
-    <header>
-
-    <nav>
-
-        <input type="checkbox" id="check">
-
-        <label for="check" class="checkbtn">
-
-            <i class="fas fa-bars"></i>
-
-        </label>
-
-        <a href="#" class="enlace">
-
-            <img src="../assets/img/LogoUni.jpg" alt="" class="logo">
-
-        </a>
-
-        <ul>
-
-            <li><a class="active" href="#">Formulario General de ingreso</a></li>
-
-           
-
-        </ul>
-
-    </nav>
-
-</header>
-
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-<Script src="../Script/gracias.js"></Script>
-
-
-
-
-</body>
-
-</html>
+?>
