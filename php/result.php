43d5229f0bdbe01d515857812d8dcbe9aea9a6f1
kevinrodri11 <kevinrodriguez1209p@gmail.com>
2022-12-28 10:53:54 -0500

    de

--- php/result.php
+++ php/result.php
@@ -72,6 +72,21 @@ $sql = "SELECT * FROM datos_personales";
     <form action="download.php" method="post">
     <button class="btn btn-success" type="submit" name="action" onclick="codigo()">Descargar Respuestas
   </button>
+  <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link" href="#" tabindex="-1">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">Next</a>
+    </li>
+  </ul>
+</nav>
+
+    
   </form>
     
 </body>
